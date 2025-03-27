e09d63639195295296b6a2acdc3409f0b9124874
Mohitjain49 <mohitkjain49@gmail.com>
2025-03-27 19:58:21 -0400

    Fixing some vue library stuff.

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Start from './pages/Start.vue';
 
+/**
+ * @type {import('vue-router').RouteRecordRaw[]} These are the routes for the application.
+ */
+const appRoutes = [
+    { path: '/', name: 'home', component: Start },
+    { path: "/:catchAll(.*)", redirect: "/" }
+]
+
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        { path: '/', name: 'home', component: Start }
-    ],
-})
+    history: createWebHistory(),
+    routes: appRoutes
+});
 
 export default router
