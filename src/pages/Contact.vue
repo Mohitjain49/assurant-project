ff7250f3f5a8103b8b2f01ba7202ce96dfa5f41a
Mohitjain49 <mohitkjain49@gmail.com>
2025-03-28 21:10:10 -0400

    Made Contact Page.

---
+++ src/pages/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+<AppNav />
+<main class="web-body">
+    <h1> Hello </h1>
+</main>
+</template>
+
+<script setup>
+import AppNav from '@/components/AppNav.vue';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    document.title = "MMI | Contact Us";
+})
+</script>
